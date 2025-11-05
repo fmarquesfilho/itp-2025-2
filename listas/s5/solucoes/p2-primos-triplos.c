4425fcfd2a97abaf970e07f2cd74cd141570d3f5
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-11-05 18:08:00 -0300

    versão otimizada dos problema dos primos triplos

--- listas/s5/solucoes/p2-primos-triplos.c
+++ listas/s5/solucoes/p2-primos-triplos.c
@@ -15,11 +15,22 @@ int primo(int n) {
 }
 
 int main() {
-    int x;
-    
-    for (x = 2; x < (50000-6); x++) {
-        if (primo(x) && primo(x+2) && primo(x+6))
-            printf("(%d, %d, %d)\n", x, x+2, x+6);
+    int x, i;
+    int v[7];
+
+    // inicializa as 7 primeiras posições do vetor
+    // 7 pois são o número de elementos no intervalo [x, x+6]
+    for (i = 0, x = 2; i < 7, x <= 8; i++, x++) {
+        v[i] = primo(x);
+    }
+
+    for (x = 2; x < 50000-6; x++) {
+        if (v[0] && v[2] && v[6])
+            printf("(%d, %d, %d)\n", x-1, x+1, x+5);
+        for (i = 1; i < 7; i++) {
+            v[i-1] = v[i];
+        }
+        v[6] = primo(x+6);
     }
 
     return 0;
