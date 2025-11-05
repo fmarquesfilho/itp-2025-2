15fbb14f1a6b951335320db5e85fcb4246858d04
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-11-05 18:50:06 -0300

    aplica correção na solução dos primos triplos

--- listas/s5/solucoes/p2-primos-triplos.c
+++ listas/s5/solucoes/p2-primos-triplos.c
@@ -20,17 +20,17 @@ int main() {
 
     // inicializa as 7 primeiras posições do vetor
     // 7 pois são o número de elementos no intervalo [x, x+6]
-    for (i = 0, x = 2; i < 7, x <= 8; i++, x++) {
-        v[i] = primo(x);
+    for (i = 0; i < 7; i++) {
+        v[i] = primo(2 + i);
     }
 
-    for (x = 2; x < 50000-6; x++) {
+    for (x = 1; x < 50000-6; x++) {
         if (v[0] && v[2] && v[6])
-            printf("(%d, %d, %d)\n", x-1, x+1, x+5);
+            printf("(%d, %d, %d)\n", x, x+2, x+6);
         for (i = 1; i < 7; i++) {
             v[i-1] = v[i];
         }
-        v[6] = primo(x+6);
+        v[6] = primo(x+7);
     }
 
     return 0;
