01598e8f08361573af2c02c19b67ed459aad3747
Fernando Figueira <690686+fmarquesfilho@users.noreply.github.com>
2025-11-20 11:39:06 -0300

    realiza otimização na função que verifica primalidade

--- listas/s5/solucoes/p2-primos-triplos/primo.c
+++ listas/s5/solucoes/p2-primos-triplos/primo.c
@@ -1,11 +1,13 @@
 #include "primo.h"
+#include <math.h>
 
 int primo(int n) {
-    int eh_primo = 1;
+    int eh_primo = 1, i;
     if (n == 1) {
         eh_primo = 0;
     } else {
-        for (int i = 2; (i <= n/2) && (eh_primo); i++) {
+        int sqrt_n = (int)sqrt((float)n);
+        for (i = 2; (i <= sqrt_n) && eh_primo; i++) {
             if ((n % i) == 0) {
                 eh_primo = 0;
             }
